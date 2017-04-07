77084d0fe3c4a708f1eefb9a905e086d1246a4f0
Stéphane Cottin <stephane.cottin@vixns.com>
2017-04-07 15:49:37 +0200

    upgrade marathon-plugin dependency to v1.4.1.

--- env/src/main/scala/mesosphere/marathon/example/plugin/env/EnvVarExtenderPlugin.scala
+++ env/src/main/scala/mesosphere/marathon/example/plugin/env/EnvVarExtenderPlugin.scala
@@ -11,6 +11,14 @@ class EnvVarExtenderPlugin extends RunSpecTaskProcessor with PluginConfiguration
     envVariables = (configuration \ "env").as[Map[String, String]]
   }
 
+  def taskGroup(podSpec: mesosphere.marathon.plugin.PodSpec,executor: org.apache.mesos.Protos.ExecutorInfo.Builder,taskGroup: org.apache.mesos.Protos.TaskGroupInfo.Builder): Unit = {
+
+  }
+
+  def taskInfo(appSpec: mesosphere.marathon.plugin.ApplicationSpec,builder: org.apache.mesos.Protos.TaskInfo.Builder): Unit = {
+
+  }
+
   def apply(runSpec: mesosphere.marathon.plugin.RunSpec, builder: org.apache.mesos.Protos.TaskInfo.Builder): Unit = {
     val envBuilder = builder.getCommand.getEnvironment.toBuilder
     envVariables.foreach {
--- http/build.sbt
+++ http/build.sbt
@@ -5,7 +5,7 @@ version := "1.0"
 resolvers += "Mesosphere Public Repo" at "http://downloads.mesosphere.io/maven"
 
 libraryDependencies ++= Seq(
-  "mesosphere.marathon" %% "plugin-interface" % "0.16.0-SNAPSHOT" % "provided",
+  "mesosphere.marathon" %% "plugin-interface" % "1.4.1" % "provided",
   "log4j" % "log4j" % "1.2.17" % "provided",
   "org.slf4j" % "slf4j-api" % "1.7.12" % "provided"
 )
